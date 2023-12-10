566d4b7840b6e62fb7bbdc7cd89d8731e596c52f
AntoineVerin <24255551+VerinAntoine@users.noreply.github.com>
2023-12-10 21:05:57 +0100

    add vacation list screen

--- app/src/main/java/fr/antoineverin/worktime/WtAppRoute.kt
+++ app/src/main/java/fr/antoineverin/worktime/WtAppRoute.kt
@@ -7,6 +7,7 @@ import androidx.navigation.navArgument
 import fr.antoineverin.worktime.ui.screen.EditEntryScreen
 import fr.antoineverin.worktime.ui.screen.ListEntriesScreen
 import fr.antoineverin.worktime.ui.screen.MainScreen
+import fr.antoineverin.worktime.ui.screen.vacation.ListVacationScreen
 
 fun NavGraphBuilder.wtAppRoute(appState: WtAppState) {
     composable(HOME_ROUTE) { MainScreen({ appState.navigate(it) }) }
@@ -17,8 +18,11 @@ fun NavGraphBuilder.wtAppRoute(appState: WtAppState) {
     ) { backEntryStack ->
         backEntryStack.arguments?.getInt("entryId")?.let { EditEntryScreen(it, { appState.popUp() }) }
     }
+    composable(LIST_VACATION) { ListVacationScreen({ appState.navigate(it) }) }
 }
 
 const val HOME_ROUTE = "home"
 const val LIST_ENTRIES = "entry/list"
 const val EDIT_ENTRY = "entry/edit"
+const val LIST_VACATION = "vacation/list"
+const val EDIT_VACATION = "vacation/edit"
---
+++ app/src/main/java/fr/antoineverin/worktime/ui/viewmodel/vacation/ListVacationViewModel.kt
@@ -0,0 +1,30 @@
+package fr.antoineverin.worktime.ui.viewmodel.vacation
+
+import androidx.compose.runtime.mutableStateListOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import fr.antoineverin.worktime.database.dao.VacationDao
+import fr.antoineverin.worktime.database.entities.Vacation
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class ListVacationViewModel @Inject constructor(
+    private val vacationDao: VacationDao
+): ViewModel() {
+
+    val vacations = mutableStateListOf<Vacation>()
+
+    fun fetchEntries() {
+        viewModelScope.launch {
+            vacations.addAll(vacationDao.getAllVacation())
+        }
+    }
+
+    fun deleteVacation(vacation: Vacation) {
+        viewModelScope.launch {
+            vacations.remove(vacation)
+            vacationDao.delete(vacation)
+        }
+    }
+
+}
