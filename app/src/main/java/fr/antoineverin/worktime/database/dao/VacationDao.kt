05c6ce2f750a16cc9f565ee4261d2a2be19f7617
AntoineVerin <24255551+VerinAntoine@users.noreply.github.com>
2023-12-12 18:26:17 +0100

    add query for vacations

--- app/src/main/java/fr/antoineverin/worktime/database/dao/VacationDao.kt
+++ app/src/main/java/fr/antoineverin/worktime/database/dao/VacationDao.kt
@@ -13,4 +13,7 @@ interface VacationDao: WtDao<Vacation> {
     @Query("SELECT * FROM vacation WHERE period = :period")
     suspend fun getAllFromPeriod(period: String): List<Vacation>
 
+    @Query("SELECT * FROM vacation WHERE id = :id")
+    suspend fun getVacation(id: Int): Vacation
+
 }
