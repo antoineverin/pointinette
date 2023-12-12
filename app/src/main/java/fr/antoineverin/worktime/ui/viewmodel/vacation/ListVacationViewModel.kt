88c48bec9298e704206da7531977c1729cd2e556
AntoineVerin <24255551+VerinAntoine@users.noreply.github.com>
2023-12-12 18:25:47 +0100

    add Annotation to ListVacationViewModel

--- app/src/main/java/fr/antoineverin/worktime/ui/viewmodel/vacation/ListVacationViewModel.kt
+++ app/src/main/java/fr/antoineverin/worktime/ui/viewmodel/vacation/ListVacationViewModel.kt
@@ -3,11 +3,13 @@ package fr.antoineverin.worktime.ui.viewmodel.vacation
 import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
 import fr.antoineverin.worktime.database.dao.VacationDao
 import fr.antoineverin.worktime.database.entities.Vacation
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@HiltViewModel
 class ListVacationViewModel @Inject constructor(
     private val vacationDao: VacationDao
 ): ViewModel() {
