8419d18dc3d050344b01a92ad46979ba5050035e
AntoineVerin <24255551+VerinAntoine@users.noreply.github.com>
2023-12-12 20:24:57 +0100

    fix fab button in ListVacationScreen

--- app/src/main/java/fr/antoineverin/worktime/ui/screen/vacation/ListVacationScreen.kt
+++ app/src/main/java/fr/antoineverin/worktime/ui/screen/vacation/ListVacationScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
@@ -39,8 +40,8 @@ fun ListVacationScreen(
 ) {
     Scaffold(
         floatingActionButton = {
-            IconButton(onClick = { navigate("$EDIT_VACATION/0") }) {
-                Icons.Filled.Add
+            FloatingActionButton(onClick = { navigate("$EDIT_VACATION/0") }) {
+                Icon(Icons.Filled.Add, "Add vacation")
             }
         }
     ) { paddingValues ->
