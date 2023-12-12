689596e26599b2c057cc7ed7ae62a1df768a413a
AntoineVerin <24255551+VerinAntoine@users.noreply.github.com>
2023-12-12 18:46:57 +0100

    add button to listvacation to add vacations items

--- app/src/main/java/fr/antoineverin/worktime/ui/screen/vacation/ListVacationScreen.kt
+++ app/src/main/java/fr/antoineverin/worktime/ui/screen/vacation/ListVacationScreen.kt
@@ -4,14 +4,18 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -27,23 +31,33 @@ import fr.antoineverin.worktime.ui.viewmodel.vacation.ListVacationViewModel
 import java.time.YearMonth
 import java.time.format.DateTimeFormatter
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ListVacationScreen(
     navigate: (String) -> Unit,
     viewModel: ListVacationViewModel = hiltViewModel()
 ) {
-    LazyColumn {
-        items(
-            items = viewModel.vacations,
-            key = { vacation -> vacation.id }
-        ) { it ->
-            VacationItem(
-                vacation = it,
-                delete = { viewModel.deleteVacation(it) },
-                edit = { navigate("${EDIT_VACATION}/${it.id}") }
-            )
+    Scaffold(
+        floatingActionButton = {
+            IconButton(onClick = { navigate("$EDIT_VACATION/0") }) {
+                Icons.Filled.Add
+            }
+        }
+    ) { paddingValues ->
+        LazyColumn(Modifier.height(paddingValues.calculateTopPadding())) {
+            items(
+                items = viewModel.vacations,
+                key = { vacation -> vacation.id }
+            ) { it ->
+                VacationItem(
+                    vacation = it,
+                    delete = { viewModel.deleteVacation(it) },
+                    edit = { navigate("${EDIT_VACATION}/${it.id}") }
+                )
+            }
         }
     }
+
 
     LaunchedEffect(viewModel) {
         viewModel.vacations.clear()
